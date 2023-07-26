0d203f4fc55d4b0dd14bdae48f257f44cab5d2c6
Kyle Erhabor <kyleerhabor@gmail.com>
2023-07-26 13:20:07 -0400

    Initial Commit

---
+++ Sequential/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Sequential
+//
+//  Created by Kyle Erhabor on 7/26/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
