260f467c3468b089289545290c3d3a78c20ece0a
Kyle Erhabor <kyleerhabor@gmail.com>
2026-01-11 18:50:00 -0500

    Version 1.0.0

--- Advance/Models/ImageCollection.swift
+++ Advance/Models/ImageCollection.swift
@@ -6,122 +6,17 @@
 //
 
 import Foundation
-import OrderedCollections
-import OSLog
-import SwiftUI
+import Observation
 
 extension URL {
   static let collectionDirectory = Self.dataDirectory.appending(component: "Collections")
 }
 
-struct ImageCollectionItemRoot {
-  typealias ID = UUID
-
-  let bookmark: UUID
-}
-
-extension ImageCollectionItemRoot: Equatable {
-  static func ==(lhs: Self, rhs: Self) -> Bool {
-    lhs.bookmark == rhs.bookmark
-  }
-}
-
-extension ImageCollectionItemRoot: Codable {}
+@Observable
+class ImageCollectionItemImage {}
 
 @Observable
-class ImageCollectionItemImage {
-  let bookmark: UUID
-
-  init(bookmark: UUID) {
-    self.bookmark = bookmark
-  }
-}
-
-extension ImageCollectionItemImage: Identifiable {
-  var id: UUID {
-    self.bookmark
-  }
-}
-
-struct ImageCollectionItem {
-  let root: ImageCollectionItemRoot
-  let image: ImageCollectionItemImage?
-}
-
-extension ImageCollectionItem: Equatable {
-  static func ==(lhs: Self, rhs: Self) -> Bool {
-    lhs.root == rhs.root
-  }
-}
-
-extension ImageCollectionItem: Codable {
-  init(from decoder: any Decoder) throws {
-    let container = try decoder.singleValueContainer()
-
-    self.init(
-      root: try container.decode(ImageCollectionItemRoot.self),
-      image: nil
-    )
-  }
-
-  func encode(to encoder: any Encoder) throws {
-    let root = ImageCollectionItemRoot(bookmark: root.bookmark)
-    var container = encoder.singleValueContainer()
-    try container.encode(root)
-  }
-}
-
-struct ImageCollectionDetailItem {
-  let image: ImageCollectionItemImage
-}
-
-extension ImageCollectionDetailItem: Identifiable {
-  var id: ImageCollectionItemImage.ID { image.id }
-}
-
-@Observable
-class ImageCollection: Codable {
-  // The source of truth for the collection.
-  //
-  // TODO: Remove unused bookmarks in store via items or order
-  @ObservationIgnored var items: [ImageCollectionItemRoot.ID: ImageCollectionItem]
-  @ObservationIgnored var order: OrderedSet<ImageCollectionItemRoot.ID>
-
+class ImageCollection {
   // The materialized state for the UI.
   var images = [ImageCollectionItemImage]()
-
-  init() {
-    self.items = [ImageCollectionItemRoot.ID: ImageCollectionItem]()
-    self.order = OrderedSet<ImageCollectionItemRoot.ID>()
-  }
-
-  // MARK: - Codable conformance
-
-  required init(from decoder: any Decoder) throws {
-    let container = try decoder.container(keyedBy: CodingKeys.self)
-    let roots = try container.decode([ImageCollectionItem].self, forKey: .items)
-
-    self.items = .init(uniqueKeysWithValues: roots.map { ($0.root.bookmark, $0) })
-    self.order = try container.decode(OrderedSet<ImageCollectionItemRoot.ID>.self, forKey: .order)
-  }
-
-  func encode(to encoder: any Encoder) throws {
-    var container = encoder.container(keyedBy: CodingKeys.self)
-    try container.encode(Array(items.values), forKey: .items)
-    try container.encode(order, forKey: .order)
-  }
-
-  enum CodingKeys: CodingKey {
-    case items, order, current
-  }
-}
-
-extension ImageCollection: Hashable {
-  static func ==(lhs: ImageCollection, rhs: ImageCollection) -> Bool {
-    lhs.order == rhs.order
-  }
-
-  func hash(into hasher: inout Hasher) {
-    hasher.combine(order)
-  }
 }
